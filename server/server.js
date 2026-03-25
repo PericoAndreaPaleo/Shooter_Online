66089f3af25556e9f3787b2d89600581853a71b7
PERICO ANDREA <PERICO.21048>
2026-03-25 10:48:18 +0100

    ModificaHitbox pugni

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ const FIRE_COOLDOWN_MS = {
 const MELEE_RANGE = {
     gun:   null,
     pistol: null,
-    fists:  80,
+    fists:  100,
 };
 
 /** Munizioni massime per ogni arma (0 = arma corpo a corpo, infinita) */
